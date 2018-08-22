1253c51680e9c6157d06dcd627be7991db4ab180
Basil Peace <grv87@yandex.ru>
2018-08-23 01:26:55 +0300

    style: avoid using assert in test sources in Groovy

--- src/test/groovy/org/fidata/gradle/utils/VersionUtilsTest.groovy
+++ src/test/groovy/org/fidata/gradle/utils/VersionUtilsTest.groovy
@@ -39,7 +39,7 @@ class VersionUtilsTest {
   @Parameters
   @TestCaseName('{index}: isPreReleaseVersion({0}) == {1}')
   void testIsPreReleaseVersion(final String version, final Boolean expectedResult) {
-    assert expectedResult == VersionUtils.isPreReleaseVersion(version)
+    expectedResult == VersionUtils.isPreReleaseVersion(version)
   }
 
   static Object[] parametersForTestIsPreReleaseVersion() {
