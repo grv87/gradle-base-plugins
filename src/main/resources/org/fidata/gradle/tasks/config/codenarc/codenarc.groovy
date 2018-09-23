e119590585f22ecf054f9a2cbea130267904874f
Basil Peace <grv87@yandex.ru>
2018-09-23 12:14:23 +0300

    chore(project): set package name in CodeNarc config
    IDEA violation

--- src/main/resources/org/fidata/gradle/tasks/config/codenarc/codenarc.groovy
+++ src/main/resources/org/fidata/gradle/tasks/config/codenarc/codenarc.groovy
@@ -17,6 +17,7 @@
  * implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+package org.fidata.gradle.tasks.config.codenarc
 
 ruleset {
   ruleset('rulesets/basic.xml')
